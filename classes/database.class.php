01a5c11db58abb489455c2d65f1b1af791a28fc6
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-18 01:29:09 +0800

    Create the database class to manage db connection using a singleton pattern and marking it final

--- classes/database.class.php
+++ classes/database.class.php
@@ -1,3 +1,39 @@
 <?php
 
-class Database {}
+declare(strict_types=1);
+
+// I am marking this class as final so you know you dont want to extend here :P
+final class Database
+{
+    private static ?PDO $instance = null;
+
+    private function __construct() {}
+
+    public static function getConnection(array $config = []): PDO
+    {
+        if (self::$instance !== null) return self::$instance;
+
+        if (empty($config)) {
+            throw new Exception("Database config missing on first initialization");
+        }
+
+
+        try {
+            $db = $config['database'];
+            $dsn = "mysql:host={$db['db_host']};dbname={$db['db_name']};charset={$db['charset']}";
+
+            self::$instance = new PDO($dsn, $db['db_user'], $db['db_pass'], $config['pdo_options'] ?? []);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection to db failed: " . $e->getMessage());
+        }
+
+        return self::$instance;
+    }
+
+    // Prevent cloning of instance
+    private function __clone() {}
+
+    // Prevent unserialization backdoor
+    public function __wakeup() {}
+}
