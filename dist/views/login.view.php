1393a64e66567246faa10248bb3fc9a4156df09b
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-19 01:18:09 +0800

    Added a bit of style to the login. Added also a JS to autohide the error

--- dist/views/login.view.php
+++ dist/views/login.view.php
@@ -5,18 +5,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Timetracker v2 - Login Page</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        table {
+            background-color: #1e1e1e;
+            color: #e0e0e0;
+            border-collapse: collapse;
+            border-radius: 5px;
+            width: 300px;
+        }
+
+        th {
+            background-color: #333;
+            color: #00d4ff;
+            /* Techy blue accent */
+            padding: 15px;
+            text-align: left;
+        }
+
+        td {
+            padding: 12px;
+            border-bottom: 1px solid #444;
+        }
+
+        tr:hover {
+            background-color: #2a2a2a;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px 15px;
+            border-radius: 10px;
+        }
+
+        input:focus {
+            outline: none;
+        }
+
+        input:hover {
+            border: 2px inset yellow;
+        }
+
+        button {
+            padding: 5px 30px;
+        }
+    </style>
 </head>
 
-<body>
+<body onload="clearError();">
     <center>
         <form action="../includes/login.inc.php" method="post">
             <table>
                 <tr>
-                    <td><?= $test; ?></td>
-                </tr>
-                <tr>
                     <td>
-                        <h1>Login</h1>
+                        <h1 style="text-align: center;">Login</h1>
                     </td>
                 </tr>
                 <tr>
@@ -34,9 +79,23 @@
                 <tr>
                     <td><button type="submit" name="submit">Login</button></td>
                 </tr>
+                <tr>
+                    <td id="error_handle"><?= $test; ?></td>
+                </tr>
             </table>
         </form>
     </center>
+
+    <script>
+        function clearError() {
+            const errorHandler = document.getElementById('error_handle');
+
+            setTimeout(() => {
+                errorHandler.innerText = '';
+            }, 10000);
+        }
+    </script>
+
 </body>
 
 </html>
