c6ed69a681a61de8d1795964ae5a9ea296668324
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-19 00:40:45 +0800

    started working on the login class for input validations

--- classes/login.class.php
+++ classes/login.class.php
@@ -2,10 +2,38 @@
 
 class Login
 {
-    public function __construct(private PDO $connection) {}
+    public function __construct(
+        private string $email,
+        private string $password,
+        private PDO $connection
+    ) {}
 
-    public function test()
+    public function loginUser(): void
     {
-        return $this->connection;
+        // check empty inputs
+        if ($this->isInputEmpty()) {
+            $this->redirectWithError("emptyinput");
+        }
+
+        // check for valid email
+        if ($this->isNotValidEmail()) {
+            $this->redirectWithError("invalidemail");
+        }
+    }
+
+    private function isInputEmpty(): bool
+    {
+        return (empty($this->email) || empty($this->password));
+    }
+
+    private function isNotValidEmail(): bool
+    {
+        return !filter_var($this->email, FILTER_VALIDATE_EMAIL);
+    }
+
+    private function redirectWithError(string $error): void
+    {
+        header("Location: ../dist/index.php?error=$error");
+        exit();
     }
 }
