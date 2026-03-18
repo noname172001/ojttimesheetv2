0b2c870c275c31dbccc00be90adec244d9a72070
larakielcode <4220069+larakielcode@users.noreply.github.com>
2026-03-18 22:21:05 +0800

    revised to return the connection object

--- includes/connection.php
+++ includes/connection.php
@@ -2,3 +2,5 @@
 require_once __DIR__ . '/../classes/database.class.php';
 $config = require __DIR__ . '/../includes/config.php';
 $connection = Database::getConnection($config);
+
+return $connection;
